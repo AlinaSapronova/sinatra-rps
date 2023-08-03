3d31f61a603d6afd394fa323478b8eaab6e4eb2f
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-03 00:57:51 +0000

    created home page and rock path

--- app.rb
+++ app.rb
@@ -2,8 +2,10 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:home)
+end
+
+get("/rock") do
+  @game = [rock,paper,scissors].sample
+  erb(:rock)
 end
