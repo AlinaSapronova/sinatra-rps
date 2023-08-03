9e1ffe28a73e95c9551006b462ce2e7065eabc0a
Alina Sapronova <106851105+AlinaSapronova@users.noreply.github.com>
2023-08-03 01:39:27 +0000

    finished project ehhuu!!!!

--- app.rb
+++ app.rb
@@ -6,6 +6,16 @@ erb(:home)
 end
 
 get("/rock") do
-  @game = [rock,paper,scissors].sample
+  @game = ["rock","paper","scissors"].sample
   erb(:rock)
+end
+
+get("/paper") do
+  @game = ["rock","paper","scissors"].sample
+  erb(:paper)
+end
+
+get("/scissors") do
+  @game = ["rock","paper","scissors"].sample
+  erb(:scissors)
 end
